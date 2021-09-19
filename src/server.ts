8e3bac4a9bb896cf87477ad0b8f67459e9660b94
axtck <alexandertack@hotmail.be>
2021-09-19 17:11:28 +0200

    Add mq connection

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import api from "./api";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
+import amqp from "amqplib/callback_api";
 
 const app = express();
 
@@ -12,6 +13,24 @@ app.use(morgan("short"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+amqp.connect("amqp://localhost", (error, connection) => {
+    if (error) throw new Error("connect");
+
+    connection.createChannel((error2, channel) => {
+        if (error2) throw new Error("createChannel");
+
+        const queue = "temperatures";
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from("Test"));
+        console.log("Sent");
+    });
+
+
+});
 
 app.get("/", (req, res) => {
     res.json({
